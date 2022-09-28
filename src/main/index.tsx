1d85bfa670683a4b89d1e48d3afcce24e14e925e
mateus <mateusflorencio@outlook.com.br>
2022-09-27 23:45:09 -0300

    add scss global

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import { InitalPage } from '@/presentation/pages'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import '@/presentation/styles/global.scss'
 
 const container = document.getElementById('main')
 const root = createRoot(container)
