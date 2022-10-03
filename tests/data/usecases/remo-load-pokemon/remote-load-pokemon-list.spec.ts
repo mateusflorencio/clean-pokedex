60d19bf4a2e63bf4f2cdcd59d316e37c66f9ec8e
mateus <mateusflorencio@outlook.com.br>
2022-10-03 13:48:54 -0300

    test: test IHttpClient in RemoteLoadPokemonListResult

--- tests/data/usecases/remo-load-pokemon/remote-load-pokemon-list.spec.ts
+++ tests/data/usecases/remo-load-pokemon/remote-load-pokemon-list.spec.ts
@@ -5,18 +5,22 @@ import { mockHttpResponse } from '@/tests/data/mocks'
 import { IHttpClient } from '@/data/protocols'
 
 const fakeUrl = faker.internet.url()
+const responseHttpClient = mockHttpResponse()
 
 describe('RemoteLoadPokemonListResult', () => {
   let sut: RemoteLoadPokemonListResult
   let httpClient: jest.Mocked<IHttpClient>
 
   beforeEach(() => {
-    httpClient = { request: jest.fn().mockReturnValue(mockHttpResponse()) }
+    httpClient = { request: jest.fn().mockReturnValue(responseHttpClient) }
     sut = new RemoteLoadPokemonListResult(fakeUrl, httpClient)
   })
 
-  it('should call httpClient with correct values', async () => {
+  it('test IHttpClient', async () => {
     await sut.listAll()
     expect(httpClient.request).toHaveBeenCalledWith({ url: fakeUrl, method: 'get' })
+    expect(httpClient.request).toHaveReturnedWith(responseHttpClient)
+    httpClient.request.mockRejectedValueOnce(new Error())
+    expect(httpClient.request).rejects.toThrowError()
   })
 })
