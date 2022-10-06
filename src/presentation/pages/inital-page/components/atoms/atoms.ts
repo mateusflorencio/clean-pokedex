a1e8c957d186aefdcb4d327557101127ae34f872
mateus <mateusflorencio@outlook.com.br>
2022-10-05 21:32:03 -0300

    add state in atom

--- src/presentation/pages/inital-page/components/atoms/atoms.ts
+++ src/presentation/pages/inital-page/components/atoms/atoms.ts
@@ -1,11 +1,16 @@
 import { Pokemon } from '@/data/models'
+import { UrlPokemon } from '@/domain/models'
 import { atom } from 'recoil'
 
-export const listPokemonState = atom({
-  key: 'listPokemonState',
+export const stateInitalPage = atom({
+  key: 'stateInitalPage',
   default: {
-    pokemons: [] as Pokemon[],
+    result: [] as UrlPokemon[],
     offset: '0',
-    limit: '20'
+    limit: '20',
+    pokemons: [] as Pokemon[],
+    count: 0,
+    next: '',
+    previous: ''
   }
 })
